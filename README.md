14fdc0669a01d084e7080b1a152dc93c174a3357
Chrovo <chrovo1@gmail.com>
2022-05-07 13:08:33 -0400

    Made buttons better and added Menu

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chrovo.github.io
+This is my personal website
+
+Currently, it is made with `HTML`, `TailwindCSS`, and `JavaScript`.
+
--- script.js
+++ script.js
@@ -19,10 +19,10 @@ function editButton() {
     let darkModeButton = document.getElementById("darkMode-btn");
     let dmDot = document.getElementById("dm-dot");
     if(htmlEl.classList.contains("dark")) {
-        darkModeButton.className = "bg-gray-200 h-6 w-11 border-2 rounded-full cursor-pointer";
+        darkModeButton.className = "bg-gray-300 h-6 w-11 border-2 rounded-full cursor-pointer";
         dmDot.className = "translate-x-5 inline-block h-5 w-5 rounded-full bg-white shadow transition ease-in-out duration-200";
     } else {
         darkModeButton.className = "bg-gray-700 h-6 w-11 border-2 rounded-full cursor-pointer";
-        dmDot.className = "translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transition ease-in-out duration-200";
+        dmDot.className = "translate-x-0 inline-block h-5 w-5 rounded-full bg-gray-500 shadow transition ease-in-out duration-200";
     }
 }
